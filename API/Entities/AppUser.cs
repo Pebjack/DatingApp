e8c6a52d07b008570dfa073eeec8ab8557b5062e
pebjack <pk.jackowski@gmail.com>
2020-12-02 14:58:46 +0100

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,6 +5,9 @@ namespace Entities
         public int Id { get; set; }
         public string UserName { get; set; }
 
-        
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+
     } 
 }
